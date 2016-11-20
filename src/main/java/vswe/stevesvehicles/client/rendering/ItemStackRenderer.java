d4ba77b51454e6efaca4a6fcc15f41b79ae99de8
Nedelosk <nedelosk@gmail.com>
2016-11-20 20:55:43 +0100

    Start to fix the buoy item rendering

--- src/main/java/vswe/stevesvehicles/client/rendering/ItemStackRenderer.java
+++ src/main/java/vswe/stevesvehicles/client/rendering/ItemStackRenderer.java
@@ -30,12 +30,7 @@ public class ItemStackRenderer extends TileEntityItemStackRenderer {
 			GL11.glPushMatrix();
 			GL11.glEnable(GL11.GL_ALPHA_TEST);
 			GL11.glScalef(-1.0F, -1.0F, 1.0F);
-			/*
-			 * if (type == ItemRenderType.EQUIPPED) { GL11.glTranslatef(0, -1,
-			 * 1); } else if (type == ItemRenderType.INVENTORY) {
-			 * GL11.glTranslatef(0, 0.5F, 0); GL11.glScalef(0.65F, 0.65F,
-			 * 0.65F); }
-			 */
+			 
 			ResourceHelper.bindResource(BuoyType.getType(itemStack.getItemDamage()).getTexture());
 			model.render(null, 0, 0, 0, 0, 0, 0.0625F);
 			GL11.glPopMatrix();
@@ -45,9 +40,9 @@ public class ItemStackRenderer extends TileEntityItemStackRenderer {
 			GL11.glScalef(-1.0F, -1.0F, 1.0F);
 			/*
 			 * if (type == ItemRenderType.EQUIPPED) { GL11.glTranslatef(0, -1,
-			 * 1); } else if (type == ItemRenderType.INVENTORY) {
-			 * GL11.glTranslatef(0, 0.1F, 0); }
-			 */
+			 * 1); } else if (type == ItemRenderType.INVENTORY) {*/
+			 GL11.glTranslatef(0, 0.1F, 0); 
+			 
 			List<ModuleData> modules = ModuleDataItemHandler.getModulesFromItem(itemStack);
 			if (modules != null) {
 				HashMap<String, ModelVehicle> models = new HashMap<>();
@@ -68,11 +63,8 @@ public class ItemStackRenderer extends TileEntityItemStackRenderer {
 						}
 					}
 				}
-				/*
-				 * if (type == ItemRenderType.INVENTORY) {
-				 * GL11.glScalef(lowestMultiplier, lowestMultiplier,
-				 * lowestMultiplier); }
-				 */
+				
+				GL11.glScalef(lowestMultiplier, lowestMultiplier, lowestMultiplier); 
 				for (ModelVehicle model : models.values()) {
 					model.render(null, 0, 0, 0, 0.0625F, 0);
 				}
