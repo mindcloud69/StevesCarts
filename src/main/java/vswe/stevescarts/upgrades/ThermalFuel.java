cdb3a062d2c04d66e7e725a5cb1e1305c5768ba2
modmuss50 <modmuss50@gmail.com>
2017-04-19 17:18:42 +0100

    Prep for first 1.11.2 alpha #alphabuild

--- src/main/java/vswe/stevescarts/upgrades/ThermalFuel.java
+++ src/main/java/vswe/stevescarts/upgrades/ThermalFuel.java
@@ -21,17 +21,16 @@ public class ThermalFuel extends TankEffect {
 	@Override
 	public void update(final TileEntityUpgrade upgrade) {
 		super.update(upgrade);
-		//TODO 1.11
-//		if (!upgrade.getWorld().isRemote && upgrade.getMaster() != null && upgrade.getFluid() != null && upgrade.getFluid().getFluid().equals(FluidRegistry.LAVA)) {
-//			final int fuelspace = upgrade.getMaster().getMaxFuelLevel() - upgrade.getMaster().getFuelLevel();
-//			final int unitspace = Math.min(fuelspace / 3, 200);
-//			if (unitspace > 100) {
-//				final FluidStack drain = upgrade.drain(unitspace, false);
-//				if (drain != null && drain.amount > 0) {
-//					upgrade.getMaster().setFuelLevel(upgrade.getMaster().getFuelLevel() + drain.amount * 3);
-//					upgrade.drain(unitspace, true);
-//				}
-//			}
-//		}
+		if (!upgrade.getWorld().isRemote && upgrade.getMaster() != null && upgrade.tank.getFluid() != null && upgrade.tank.getFluid().getFluid().equals(FluidRegistry.LAVA)) {
+			final int fuelspace = upgrade.getMaster().getMaxFuelLevel() - upgrade.getMaster().getFuelLevel();
+			final int unitspace = Math.min(fuelspace / 3, 200);
+			if (unitspace > 100) {
+				final FluidStack drain = upgrade.tank.drain(unitspace, false);
+				if (drain != null && drain.amount > 0) {
+					upgrade.getMaster().setFuelLevel(upgrade.getMaster().getFuelLevel() + drain.amount * 3);
+					upgrade.tank.drain(unitspace, true);
+				}
+			}
+		}
 	}
 }
